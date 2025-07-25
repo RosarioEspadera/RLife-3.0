d92a9da5140bb7901a85f4f5fe10a9f7a2707177
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-26 01:10:56 +0800

    Create traitAffinity.js

---
+++ engine/data/traitAffinity.js
@@ -0,0 +1,5 @@
+export const traitAffinity = {
+  "Biologist Lando": { curious: 0.9, sweet: 0.6, jaded: 0.1 },
+  "Cousin Bea": { sweet: 0.8, curious: 0.4, jaded: 0.2 },
+  "Crush Migs": { jaded: 0.7, sweet: 0.3, curious: 0.5 }
+};
