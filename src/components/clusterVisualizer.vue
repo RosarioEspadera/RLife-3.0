24726746f1b7cb544c3d0ff481031a78fcad3816
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-26 05:07:44 +0800

    Create clusterVisualizer.vue

---
+++ src/components/clusterVisualizer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="clusterVisualizer">
+    <div v-for="cluster in clusters" :key="cluster.tag" class="clusterBlock">
+      <h3>{{ cluster.tag }}</h3>
+      <ul>
+        <li v-for="mem in cluster.memories" :key="mem.timestamp">
+          {{ formatMemory(mem) }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { getMemoryClusters } from '../data/memoryCluster.js';
+const clusters = getMemoryClusters(props.npc);
+function formatMemory(mem) {
+  return `${new Date(mem.timestamp).toLocaleDateString()}: ${mem.event}`;
+}
+</script>
