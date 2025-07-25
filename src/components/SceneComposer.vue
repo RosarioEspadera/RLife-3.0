3bca7ef203c7b59e6bbaa8f8d129fc9933f9e50d
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-26 05:11:46 +0800

    Update SceneComposer.vue

--- src/components/SceneComposer.vue
+++ src/components/SceneComposer.vue
@@ -1,1 +1,6 @@
-
+{
+  "sceneId": "moonlitFairFlashback",
+  "trigger": "loveCluster",
+  "panelArt": "moonlit_fair_hands.png",
+  "caption": "A soft breeze, faint laughter, and two hands that didn't mean to touch."
+}
