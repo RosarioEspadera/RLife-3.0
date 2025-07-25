930917d02852587ee4cd9db1d7b2ea916e07732f
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-26 01:06:07 +0800

    Create npcTraits.js

---
+++ engine/data/npcTraits.js
@@ -0,0 +1,17 @@
+export const npcTraits = {
+  sweet: {
+    reactsTo: ['Sweet', 'Romance'],
+    moodBoost: ['River Lights', 'Strawberry Date'],
+    unlocksSticker: ['baguio-sweets.png']
+  },
+  curious: {
+    reactsTo: ['Nature', 'Grounded'],
+    moodBoost: ['Eagle Encounter'],
+    unlocksSticker: ['davao-biologist.png']
+  },
+  jaded: {
+    reactsTo: ['DailyLife', 'Overstimulated'],
+    moodBoost: ['Sidewalk Serenade'],
+    unlocksSticker: ['manila-sigh.png']
+  }
+};
