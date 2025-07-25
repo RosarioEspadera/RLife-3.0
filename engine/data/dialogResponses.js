97f31f3a8c6e7569547a230cab8097ff70616974
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-26 01:08:34 +0800

    Update dialogResponses.js

--- engine/data/dialogResponses.js
+++ engine/data/dialogResponses.js
@@ -1,5 +1,12 @@
-{
-  event: "Strawberry Date",
-  sweetNPC: "You picked strawberries... for me?",
-  jadedNPC: "Must be nice to feel things like that."
-}
+export const dialogResponses = {
+  "Strawberry Date": {
+    sweet: "You picked strawberries... for me? That's adorable.",
+    jaded: "Must be nice to feel things like that.",
+    curious: "Were the strawberries cold? I read they taste better after frost."
+  },
+  "Eagle Encounter": {
+    curious: "That’s a rare one. Did you get close?",
+    sweet: "Wow… majestic. I wish I could've seen it with you.",
+    jaded: "Hope it didn’t poop on you."
+  }
+};
